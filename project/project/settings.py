788ae8f242913b6fef1f4daa3bf982a0da1075d0
sunil1206 <masunil12@gmail.com>
2025-08-13 11:26:14 +0200

    2nd

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-huo7-ub*11nr@satj^vifj+58_7*3+g#fm%r+$f3$bxp14-n^j
 DEBUG = True
 
 ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['parismagiclandtransfer.com','www.parismagiclandtransfer.com','188.166.151.139']
 
 SITE_ID = 1
 
@@ -175,5 +176,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='sunilma94@gmail.com'
-EMAIL_HOST_PASSWORD = 'zexazsyvetsnozht'
+EMAIL_HOST_USER ='samuel.fr125@gmail.com'
+EMAIL_HOST_PASSWORD = 'qdgqpgyozrdrsiuu'
+
+# zexazsyvetsnozht
